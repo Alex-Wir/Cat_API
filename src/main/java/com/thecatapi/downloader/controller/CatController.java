128505e4336cd8f2169963c0ab695f51a1045b3a
Alex-Wir <nemanwir@gmail.com>
2019-05-25 14:45:34 +0300

    add getAllByLimit()

--- src/main/java/com/thecatapi/downloader/controller/CatController.java
+++ src/main/java/com/thecatapi/downloader/controller/CatController.java
@@ -2,18 +2,26 @@ package com.thecatapi.downloader.controller;
 
 import com.thecatapi.downloader.service.CatService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
+@RequestMapping("/cats")
 @AllArgsConstructor
 public class CatController {
 
     private final CatService catService;
-	
-    @RequestMapping("/")
+
+    @RequestMapping(method = RequestMethod.GET)
     public String getOne() {
         return catService.getOneRandom();
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public Set<String> getAllWithParam(@RequestParam long limit) {
+        System.out.println(limit);
+        return catService.getAllByLimit(limit);
     }
 
 }
