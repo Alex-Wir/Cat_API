4ca66e20f30203cebf7ab7e9482241755406b9aa
Alex-Wir <nemanwir@gmail.com>
2019-05-28 12:50:14 +0300

    add validation for CatRequest, localization and MessagesConfiguration

--- src/main/java/com/thecatapi/downloader/CatDownloaderApplication.java
+++ src/main/java/com/thecatapi/downloader/CatDownloaderApplication.java
@@ -1,8 +1,11 @@
 package com.thecatapi.downloader;
 
+import com.thecatapi.downloader.config.MessagesConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(MessagesConfiguration.class)
 @SpringBootApplication
 public class CatDownloaderApplication {
 
