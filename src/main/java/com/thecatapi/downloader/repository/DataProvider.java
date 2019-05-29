431cb4d243ac98d21e3d37eecf7903648a7d6ec0
Alex-Wir <nemanwir@gmail.com>
2019-05-29 16:01:27 +0300

    move image saving logic to separate class

--- src/main/java/com/thecatapi/downloader/repository/DataProvider.java
+++ src/main/java/com/thecatapi/downloader/repository/DataProvider.java
@@ -2,47 +2,36 @@ package com.thecatapi.downloader.repository;
 
 import com.thecatapi.downloader.config.LocalizedMessageSource;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import javax.net.ssl.SSLContext;
-import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-@PropertySource("classpath:download.properties")
-@Component
+@Repository
 @Slf4j
-public class CatRepository {
-
-    //TODO add Except for zero image result
+public class DataProvider {
 
     private static final String SEARCH_URL = "https://api.thecatapi.com/v1/images/search";
     private static final String UTF8 = "UTF-8";
 
     private final LocalizedMessageSource localizedMessageSource;
-
-    @Value("${download.folder}")
-    private String folder;
+    private final LocalStorage localStorage;
 
     /**
      * Prevent javax.net.ssl.SSLHandshakeException: Received fatal alert: handshake_failure
      */
-    public CatRepository(LocalizedMessageSource localizedMessageSource) {
+    public DataProvider(LocalizedMessageSource localizedMessageSource, LocalStorage localStorage) {
         this.localizedMessageSource = localizedMessageSource;
+        this.localStorage = localStorage;
         try {
             SSLContext ctx = SSLContext.getInstance("TLSv1.2");
             ctx.init(null, null, null);
@@ -55,14 +44,11 @@ public class CatRepository {
 
     public Set<String> getAll(String request) {
         JSONArray jsonArray = getJsonArray(SEARCH_URL + request);
-        //TODO delete SOUT
-        System.out.println(SEARCH_URL + request);
         Set<String> filePaths = new HashSet<>();
         for (Object o : jsonArray) {
             JSONObject jsonObject = (JSONObject) o;
             String urlString = jsonObject.getString("url");
-            System.out.println(urlString);
-            filePaths.add(saveOne(urlString));
+            filePaths.add(localStorage.saveFileByUrl(urlString));
         }
         return filePaths;
     }
@@ -72,37 +58,9 @@ public class CatRepository {
         try {
             jsonArray = new JSONArray(IOUtils.toString(new URL(urlForSearch), Charset.forName(UTF8)));
         } catch (IOException e) {
-            log.error("Error reading JSON by URL = "+urlForSearch, e);
+            log.error("Error reading JSON by URL = " + urlForSearch, e);
             throw new RuntimeException(localizedMessageSource.getMessage("repository.download.error", new Object[]{}));
         }
         return jsonArray;
-    }
-
-    private String saveOne(String urlString) {
-
-        String fileName = "";
-        final Matcher matcher = Pattern.compile("images\\/").matcher(urlString);
-        if (matcher.find()) {
-            fileName = urlString.substring(matcher.end()).trim();
-        }
-        File newFile = new File(folder + fileName);
-
-        URL url;
-        try {
-            url = new URL(urlString);
-        } catch (MalformedURLException e) {
-            log.error("Invalid URL = "+urlString, e);
-            throw new RuntimeException(localizedMessageSource.getMessage("repository.download.error", new Object[]{}));
-        }
-
-        try {
-            FileUtils.copyURLToFile(url, newFile, 10000, 10000);
-        } catch (IOException e) {
-            log.error("Error during saving file "+fileName, e);
-            throw new RuntimeException(localizedMessageSource.getMessage("repository.download.error", new Object[]{}));
-
-        }
-
-        return (folder + fileName);
     }
 }
---
+++ src/main/java/com/thecatapi/downloader/repository/LocalStorage.java
@@ -0,0 +1,53 @@
+package com.thecatapi.downloader.repository;
+
+import com.thecatapi.downloader.config.LocalizedMessageSource;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Repository;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+@PropertySource("classpath:download.properties")
+public class LocalStorage {
+
+    private final LocalizedMessageSource localizedMessageSource;
+
+    @Value("${download.folder}")
+    private String folder;
+
+    @Value("${connection.timeout}")
+    private int connectionTimeout;
+
+    @Value("${read.timeout}")
+    private int readTimeout;
+
+    public String saveFileByUrl(String urlString) {
+
+        String fileName = urlString.substring(urlString.lastIndexOf("/") + 1);
+        System.out.println(fileName);
+
+        if (fileName.isEmpty()) {
+            log.error("Invalid file name in URL = " + urlString);
+            throw new RuntimeException(localizedMessageSource.getMessage("repository.download.error", new Object[]{}));
+        }
+
+        try {
+            URL url = new URL(urlString);
+            File file = new File(folder + fileName);
+            FileUtils.copyURLToFile(url, file, connectionTimeout, readTimeout);
+        } catch (IOException e) {
+            log.error("Error during saving file by URL = " + urlString, e);
+            throw new RuntimeException(localizedMessageSource.getMessage("repository.download.error", new Object[]{}));
+        }
+
+        return folder + fileName;
+    }
+}
