8eccaeb9f67970b279f340d2056c61ffa3211977
Alex-Wir <nemanwir@gmail.com>
2019-06-01 20:01:08 +0300

    fix LocalStorageTest - add deleteTestDownloadFolder

--- src/test/java/com/thecatapi/downloader/unit/repository/LocalStorageTest.java
+++ src/test/java/com/thecatapi/downloader/unit/repository/LocalStorageTest.java
@@ -2,11 +2,16 @@ package com.thecatapi.downloader.unit.repository;
 
 import com.thecatapi.downloader.config.LocalizedMessageSource;
 import com.thecatapi.downloader.repository.LocalStorage;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -20,31 +25,42 @@ public class LocalStorageTest {
 
     private LocalizedMessageSource localizedMessageSource;
 
+    private final static String downloadFolderForTest = "images_test/";
+
+    @BeforeEach
+    private void setTestDownloadFolder() {
+        try {
+            Field field = this.localStorage.getClass().getDeclaredField("folder");
+            field.setAccessible(true);
+            field.set(this.localStorage, downloadFolderForTest);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Test
     public void testSaveFileByUrl_jpgFile() {
         String urlJpgFile = "https://cdn2.thecatapi.com/images/e3s.jpg";
-        setTestFolderForDownload(localStorage);
         assertTrue(localStorage.saveFileByUrl(urlJpgFile).endsWith("jpg"));
     }
 
     @Test
     public void testSaveFileByUrl_wrongFileNameInUrl_shouldThrowException() {
         String wrongUrl = "google.com/";
-        assertThrows(RuntimeException.class, ()->localStorage.saveFileByUrl(wrongUrl));
+        assertThrows(RuntimeException.class, () -> localStorage.saveFileByUrl(wrongUrl));
     }
 
     @Test
     public void testSaveFileByUrl_wrongUrl_shouldThrowException() {
         String wrongUrl = "djaflskjdfl";
-        assertThrows(RuntimeException.class, ()->localStorage.saveFileByUrl(wrongUrl));
+        assertThrows(RuntimeException.class, () -> localStorage.saveFileByUrl(wrongUrl));
     }
 
-    private void setTestFolderForDownload(LocalStorage localStorage) {
+    @AfterAll
+    private static void deleteTestDownloadFolder() {
         try {
-            Field field = localStorage.getClass().getDeclaredField("folder");
-            field.setAccessible(true);
-            field.set(localStorage, "images_test/");
-        } catch (NoSuchFieldException | IllegalAccessException e) {
+            FileUtils.deleteDirectory(new File(downloadFolderForTest));
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
