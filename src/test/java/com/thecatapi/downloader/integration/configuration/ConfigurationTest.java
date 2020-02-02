14a4d12bfebb74c964e550423b66c77d9cb815ff
Alex Apon <nemanwir@gmail.com>
2020-02-02 23:19:48 +0300

    add integration tests

---
+++ src/test/java/com/thecatapi/downloader/integration/configuration/ConfigurationTest.java
@@ -0,0 +1,13 @@
+package com.thecatapi.downloader.integration.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConfigurationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
